38425142aaaa9209550961b5168cb2c2d204d10b
jaejin747 <140138881+jaejin747@users.noreply.github.com>
2025-05-14 13:19:31 +0900

    Add files via upload

---
+++ application code/kotlin/RegisterRequest.kt
@@ -0,0 +1,37 @@
+package com.BoxOffice.androidlab
+
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+
+class RegisterRequest(
+    seat: String,
+    password: String,
+    name: String,
+    phone: String,
+    listener: Response.Listener<String?>?
+) :
+    StringRequest(Method.POST, URL, listener, null) {
+    private val map: MutableMap<String, String>
+
+    @Throws(AuthFailureError::class)
+    override fun getParams(): Map<String, String>? {
+        return map
+    }
+
+    companion object {
+        //apk 배포 시
+        //private const val URL = "https://845e-210-125-112-206.ngrok-free.app/register.php"
+
+        //에뮬레이터 실행 시
+        private const val URL = "http://10.0.2.2/register.php"
+    }
+
+    init {
+        map = HashMap()
+        map["seat"] = seat
+        map["password"] = password
+        map["name"] = name
+        map["phone"] = phone
+    }
+}
